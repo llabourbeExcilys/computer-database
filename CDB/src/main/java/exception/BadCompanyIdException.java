d32de4e9d2096fa7acc7575592f01132e801d626
llabourbeExcilys <llabourbe@excilys.com>
2019-04-23 10:18:49 +0200

    serialVersionUID pour les exceptions

--- CDB/src/main/java/exception/BadCompanyIdException.java
+++ CDB/src/main/java/exception/BadCompanyIdException.java
@@ -1,6 +1,11 @@
 package exception;
 
 public class BadCompanyIdException extends RuntimeException{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public BadCompanyIdException(String message) {
 		super(message);
 	}
--- CDB/src/main/java/exception/NotFoundException.java
+++ CDB/src/main/java/exception/NotFoundException.java
@@ -2,6 +2,11 @@ package exception;
 
 public class NotFoundException extends RuntimeException{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public NotFoundException(String message) {
 		super(message);
 	}
