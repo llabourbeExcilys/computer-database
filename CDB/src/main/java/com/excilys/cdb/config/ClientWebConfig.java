14ce0870bcc2356ecb11cc6ebac14902960fa72b
llabourbeExcilys <llabourbe@excilys.com>
2019-05-20 10:28:02 +0200

    Fix du css qui ne se chargait plus

--- CDB/src/main/java/com/excilys/cdb/config/ClientWebConfig.java
+++ CDB/src/main/java/com/excilys/cdb/config/ClientWebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -14,20 +15,25 @@ import org.springframework.web.servlet.view.JstlView;
 @Configuration
 @ComponentScan("com.excilys.cdb")
 public class ClientWebConfig implements WebMvcConfigurer {
-	 
-	   @Override
-	   public void addViewControllers(ViewControllerRegistry registry) {
-	      registry.addViewController("/CDB");
-	   }
-	 
-	   @Bean
-	   public ViewResolver viewResolver() {
-	      InternalResourceViewResolver bean = new InternalResourceViewResolver();
-	 
-	      bean.setViewClass(JstlView.class);
-	      bean.setPrefix("/WEB-INF/views/");
-	      bean.setSuffix(".jsp");
-	 
-	      return bean;
-	   }
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/CDB");
 	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+	}
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver bean = new InternalResourceViewResolver();
+
+		bean.setViewClass(JstlView.class);
+		bean.setPrefix("/WEB-INF/views/");
+		bean.setSuffix(".jsp");
+
+		return bean;
+	}
+};
