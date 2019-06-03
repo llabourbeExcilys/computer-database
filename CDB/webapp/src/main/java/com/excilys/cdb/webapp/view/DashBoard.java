cf822198316bc1ac88cfddcef57690bf0123e407
llabourbeExcilys <llabourbe@excilys.com>
2019-06-03 14:21:49 +0200

    Mise en place d'attributs de session pour le controller DashBoard

--- CDB/webapp/src/main/java/com/excilys/cdb/webapp/view/DashBoard.java
+++ CDB/webapp/src/main/java/com/excilys/cdb/webapp/view/DashBoard.java
@@ -4,62 +4,74 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.excilys.cdb.binding.dto.ComputerDTO;
 import com.excilys.cdb.persistence.dao.SortingField;
 import com.excilys.cdb.persistence.dao.SortingOrder;
 import com.excilys.cdb.webapp.controller.WebController;
+import com.excilys.cdb.webapp.page.Page;
 
 @org.springframework.stereotype.Controller
 @RequestMapping("/dashboard")
+@SessionAttributes(value = "page",types = {Page.class})
 public class DashBoard {
+	
+	private static Logger logger = LoggerFactory.getLogger( DashBoard.class );
 
-	@Autowired 
 	private WebController controller;
-	private static int nbByPage = 10;
-	private static int page = 1;
-	private static String field = "";
-	private static String order = "";
-	private static SortingField sortingField;
-	private static SortingOrder sortingOrder;
+	
+	public DashBoard(WebController webController) {
+		this.controller = webController;
+	}
+	
+	@ModelAttribute("page")
+	public Page getMyPage() {
+		return new Page(10,1,SortingField.ID,SortingOrder.ASC);
+	}
 	
    @GetMapping
     public String handle(Model model,
+    		@ModelAttribute("page") Page page,
     		@RequestParam(name = "field", required = false) String fieldString,
     		@RequestParam(name = "order", required = false) String orderString,
     		@RequestParam(name = "nbByPage", required = false) String nbByPageString,
     		@RequestParam(name = "page", required = false) String pageString,
     		@RequestParam(name = "search", required = false) String computerSearch) {
-        
-	   if(fieldString!=null && !fieldString.equals(""))
-			field = fieldString;
-		if(orderString!=null && !orderString.equals(""))
-			order = orderString;
-		if(nbByPageString!=null)
-			nbByPage = Integer.parseInt(nbByPageString);
-		if(pageString!=null)
-			page = Integer.parseInt(pageString);
-
-		long nbComputerFound = controller.getNumberOfComputer();
-		int lastPage = (int) Math.ceil(nbComputerFound/(double)nbByPage) ;
-		
-		switch (field) {
-			case "name":	  sortingField = SortingField.NAME; 				break;
-			case "introDate": sortingField = SortingField.DATE_INTRODUCTION;	break;
-			case "disconDate":sortingField = SortingField.DATE_DISCONTINUATION;	break;
-			case "company":	  sortingField = SortingField.COMPANY; 				break;
-			default: 		  sortingField = SortingField.ID; 					break;
+	   
+		try {
+			if(nbByPageString!=null)
+				page.setNbByPage(Integer.parseInt(nbByPageString));
+			if(pageString!=null) 
+				page.setPage(Integer.parseInt(pageString));
+		} catch (NumberFormatException e) {
+			logger.info(e.getMessage());
 		}
-		switch (order) {
-			case "asc": sortingOrder = SortingOrder.ASC;  break;
-			case "desc":sortingOrder = SortingOrder.DESC; break;
-			default: 	sortingOrder = SortingOrder.ASC;  break;
-		}	
+
+		if(fieldString!=null && !fieldString.equals("")) {
+			switch (fieldString) {
+				case "name":	  page.setSortingField(SortingField.NAME); break;
+				case "introDate": page.setSortingField(SortingField.DATE_INTRODUCTION);	break;
+				case "disconDate":page.setSortingField(SortingField.DATE_DISCONTINUATION); break;
+				case "company":	  page.setSortingField(SortingField.COMPANY); break;
+				default: 		  page.setSortingField(SortingField.ID); break;
+			}
+		}
+		
+		if(orderString!=null && !orderString.equals("")) {
+			switch (orderString) {
+			case "asc": page.setSortingOrder(SortingOrder.ASC); break;
+			case "desc":page.setSortingOrder(SortingOrder.DESC); break;
+			default: 	page.setSortingOrder(SortingOrder.ASC); break;
+			}
+		}		
 		
 		List<ComputerDTO> computers	= new ArrayList<>();
 		if(computerSearch!=null) {
@@ -67,15 +79,18 @@ public class DashBoard {
 			if (optComputer.isPresent())
 				computers.add(optComputer.get());
 		}else {
-			computers = controller.getComputerPage(page, nbByPage,sortingField, sortingOrder);
+			computers = controller.getComputerPage(page.getPage(), page.getNbByPage(),page.getSortingField(), page.getSortingOrder());
 		}
+
+		long nbComputerFound = controller.getNumberOfComputer();
+		int lastPage = (int) Math.ceil(nbComputerFound/(double)page.getNbByPage()) ;
 		
 		model.addAttribute("nbComputerFound", nbComputerFound);
 		model.addAttribute("lastPage", lastPage);
-		model.addAttribute("page", page);
+		model.addAttribute("page", page.getPage());
 		model.addAttribute("computers", computers);
 		
         return "dashboard";
     }
+ }
    
-}
